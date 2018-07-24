116c6dd8476f1087fbb88d3072932533f20e6c38
kshah21 <kshah2121@gmail.com>
2018-07-24 17:14:21 -0500

    Create two modules

---
+++ Nomination/src/main/java/com/solstice/nomination/Nomination/NominationApplication.java
@@ -0,0 +1,12 @@
+package com.solstice.nomination.Nomination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NominationApplication.class, args);
+	}
+}

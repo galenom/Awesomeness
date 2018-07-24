81d827d043f0ee2a840702f7a12a97ba9f4539c5
kshah21 <kshah2121@gmail.com>
2018-07-24 16:59:22 -0500

    Set up SQL Schema

---
+++ src/main/java/com/solstice/awesomeness/AwesomenessNomination/AwesomenessNominationApplication.java
@@ -0,0 +1,12 @@
+package com.solstice.awesomeness.AwesomenessNomination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomenessNominationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomenessNominationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/solstice/awesomeness/AwesomenessNomination/AwesomenessNominationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.solstice.awesomeness.AwesomenessNomination;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwesomenessNominationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
